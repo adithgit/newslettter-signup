66e99675e4bd4a2e1a659f378d3f61685286a38a
otto <5285@mbcpeermade.com>
2022-01-19 01:08:18 +0530

    ready

--- index.js
+++ index.js
@@ -1,31 +1,77 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const request = require('request')
-
+const https = require('https')
+const path = require('path')
 const app = express();
-// var mailChimp = 'https://us19.admin.mailchimp.com/159e872d992a80079f0ea7d1ac5b5adf-us20'; 
 
-app.use(bodyParser({urlencoded:'true'}));
 
-app.get('/',(req,res)=>{
-    res.sendFile(__dirname+"/signup.html")
+app.use(bodyParser({ urlencoded: 'true' }));
+
+//Homepage 
+
+app.get('/', (req, res) => {
+    const signupPath = path.resolve(__dirname, "signup.html");
+    res.sendFile(signupPath);
 })
 
-app.post('/',(req,res)=>{
-    var firstName = req.body.firstName;
-    var secondNamr = req.body.secondName;
-    var email = req.body.email
-    console.log(email);
-    res.send("Data recorded")
+//Mailchimp URL & options to be used inside https request 
+
+const URL = 'https://us20.api.mailchimp.com/3.0/lists/af338bae2b/members'
+const options = {
+    method: 'POST',
+    auth: 'key:159e872d992a80079f0ea7d1ac5b5adf-us20',
+    header: {
+        'content-type': 'application/json'
+    }
+}
+
+
+//Post request -> Sending data to Mailchimp via https request 
+
+app.post('/', (req, res) => {
+
+    //Input data given by the user
+
+    const firstName = req.body.firstName;
+    const secondName = req.body.lastName;
+    const email = req.body.email;
+
+    // Data to be sent
+
+    const dataToSend = {
+        "email_address": email,
+        "status": "subscribed",
+        "merge_fields": {
+            "FNAME": firstName,
+            "LNAME": secondName
+        }
+    };
+    var jsonData = JSON.stringify(dataToSend);
+    
+    const request = https.request(URL, options, (response) => {
+        
+        if (response.statusCode === 200) {
+            const successPath = path.resolve(__dirname, 'success.html');
+            res.sendFile(successPath);
+        }else {
+            const failurePath = path.resolve(__dirname, 'failure.html');
+            res.sendFile(failurePath);
+        }
+
+        response.on("data", (d) => {
+            console.log(JSON.parse(d));
+        })
+
+        response.on('end', () => {
+            console.log("API request finished");
+        })
+    })
+
+
+    request.write(jsonData);
+    request.end();
 })
 
-app.listen(3000,()=>{
+app.listen(3000, () => {
     console.log('Listening on 3000');
 })
-
-
-//Audience ID
-// af338bae2b
-
-//API KEy
-// 159e872d992a80079f0ea7d1ac5b5adf-us20
